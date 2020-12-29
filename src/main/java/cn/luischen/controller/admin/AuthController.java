69d516e7a3fbb29dd1eb7fba344ab75890f7d673
liupan <liupanjjjj@gmail.com>
2020-12-29 14:03:31 +0800

    解决依赖冲突

--- src/main/java/cn/luischen/controller/admin/AuthController.java
+++ src/main/java/cn/luischen/controller/admin/AuthController.java
@@ -15,8 +15,9 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -36,8 +37,7 @@ import java.io.IOException;
 @RequestMapping(value = "/admin")
 public class AuthController extends BaseController{
 
-    private static final Logger LOGGER = LogManager.getLogger(AuthController.class);
-
+    private final static Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
     @Autowired
     private UserService userService;
 
